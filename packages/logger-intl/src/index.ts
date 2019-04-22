62ca6dbd61d838acbfdbb7785a216e7dfb62ee55
jarvis <jiashunming@outlook.com>
2019-04-22 22:06:47 +0800

    refactor: 调整获取logger client的方式

--- packages/logger-intl/src/index.ts
+++ packages/logger-intl/src/index.ts
@@ -7,17 +7,19 @@ export interface LoggerIntl {
   logPath: string
   isLocal?: boolean
   cache?: Cache<Loggers>
+  getLogger: (logType: string) => LoggerClient
+  buildLogger: (logType: string) => Loggers
+}
+
+export interface LoggerClient {
   access: (data?: Record<string, any>) => void
   error: (data?: Record<string, any>) => void
-  buildLogger: (logType: string) => Loggers
 }
 
 export default abstract class Logger implements LoggerIntl {
   public logType: string
 
   private colors: any
-
-  private loggers: Loggers
 
   constructor (
     public logPath: string,
@@ -29,27 +31,33 @@ export default abstract class Logger implements LoggerIntl {
     }
   }
 
-  access (data?: Record<string, any>) {
-    if (this.isLocal) {
-      console.log(this.colors.green(data))
-    }
-    this.loggers.accessLog(data)
-  }
+  getLogger (logType: string) {
+    const logger = this.buildLogger(logType)
+    const { colors } = this
 
-  error (error: Error, data?: Record<string, any>) {
-    const err = {
-      err_msg: error.message,
-      err_name: error.name,
-      err_stack: error.stack,
-    }
+    return {
+      access (data?: Record<string, any>) {
+        if (this.isLocal) {
+          console.log(colors.green(data))
+        }
+        logger.accessLog(data)
+      },
+      error (error: Error, data?: Record<string, any>) {
+        const err = {
+          err_msg: error.message,
+          err_name: error.name,
+          err_stack: error.stack,
+        }
 
-    const results = { ...err, ...data }
+        const results = { ...err, ...data }
 
-    // 添加本地环境的colors输出
-    if (this.isLocal) {
-      console.error(this.colors.red(results))
+        // 添加本地环境的colors输出
+        if (this.isLocal) {
+          console.error(colors.red(results))
+        }
+        logger.errorLog(results)
+      },
     }
-    this.loggers.errorLog(results)
   }
 
   abstract buildLogger (logType: string): Loggers
