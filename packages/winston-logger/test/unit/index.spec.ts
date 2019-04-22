65f36939a19bed8cd41048b63ceb4c532c97a1cf
jarvis <jiashunming@outlook.com>
2019-04-22 22:09:02 +0800

    feat: 修改获取logger的方式

--- packages/winston-logger/test/unit/index.spec.ts
+++ packages/winston-logger/test/unit/index.spec.ts
@@ -3,8 +3,9 @@ import Logger from '../../src/index'
 
 describe('normal', () => {
   it(`output logs`, done => {
-    const logger = new Logger('logs', 'test', new Map())
+    const loggerClient = new Logger('logs', new Map())
     try {
+      const logger = loggerClient.getLogger('test')
       logger.access({ msg: 'test' })
       logger.error(new Error('test error'), { msg: 'test' })
     } catch (e) {
