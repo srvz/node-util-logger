d414a7fb87cd9b183cba67a9dcc50e730b77071d
zhf <zhanghefei@blued.com>
2021-05-28 19:13:51 +0800

    refactor: add config by environment variables

--- packages/winston-logger/README.md
+++ packages/winston-logger/README.md
@@ -24,6 +24,20 @@ Argument|Type|Required|Desc
 `logType`|`string`|✅|日志的标识符`YYYY-MM-DD-%logType%.<access|error>.log`
 `isLocal`|`string`|❌|是否为本地环境，开启 terminal 输出，默认为`false`
 
+### environment variables
+
+Argument|Type|Required|Desc
+:--|:--|:--|:--
+`LOG_META_SERVICE`|`string`|❌|在日志 meta 里添加 service 字段，默认不添加
+`LOG_CONSOLE`|`string`|❌|是否开启输出日志到 console，默认关闭。值为 `true` 时开启
+`LOG_FILE`|`string`|❌|是否关闭输出日志到文件，默认开启。值为 `false` 时关闭输出到文件，LOG_FILE 前缀变量均失效
+`LOG_FILE_DIRNAME`|`string`|❌|日志输出的文件夹路径，必须为绝对路径，会覆盖 `logPath` 的设置
+`LOG_FILE_ACCESS_FILENAME`|`string`|❌|访问日志的文件名标识符，默认为 `%DATE%-${logType}.access.log`
+`LOG_FILE_ERROR_FILENAME`|`string`|❌|错误日志的文件名标识符，默认为 `%DATE%-${logType}.error.log`
+`LOG_FILE_DATE_PATTERN`|`string`|❌|日志文件的日期前缀格式，默认为 `YYYY-MM-DD`
+`LOG_FILE_MAX_FILES`|`string`|❌|日志最长保存时间，默认 `30d` 30 天
+`LOG_FILE_MAX_SIZE`|`string`|❌|日志文件的最大尺寸，默认为 `null`，不限制，可选择为 `1g` `100m`
+
 ### access
 
 Argument|Type|Required|Desc
--- packages/winston-logger/src/index.ts
+++ packages/winston-logger/src/index.ts
@@ -1,6 +1,5 @@
 import Logger from '@blued-core/logger-intl'
 import fs from 'fs'
-import path from 'path'
 import mkdirp from 'mkdirp'
 import winston from 'winston'
 import 'winston-daily-rotate-file'
@@ -13,31 +12,68 @@ export default class extends Logger {
   buildLogger (logType: string) {
     if (this.cache.has(logType)) return this.cache.get(logType)
 
-    const { logPath } = this
-    if (!fs.existsSync(logPath)) {
-      mkdirp.sync(logPath)
-    }
+    const {
+      LOG_META_SERVICE: logMetaService,
+      LOG_CONSOLE: logConsole,
+      LOG_FILE: logFile,
+      LOG_FILE_DIRNAME: logFileDirname,
+      LOG_FILE_ACCESS_FILENAME: logFileAccessFilename,
+      LOG_FILE_ERROR_FILENAME: logFileErrorFilename,
+      LOG_FILE_DATE_PATTERN: logFileDatePattern,
+      LOG_FILE_MAX_FILES: logFileMaxFiles,
+      LOG_FILE_MAX_SIZE: logFileMaxSize,
+    } = process.env
 
-    const accessLog = winston.createLogger({
-      transports: [
-        new (winston.transports as DailyRotateFile).DailyRotateFile({
-          filename: path.resolve(logPath, `./%DATE%-${logType}.access.log`),
-          datePattern: `YYYY-MM-DD-HH`,
+    const accessTransports = []
+    const errorTransports = []
+
+    accessTransports.push(new winston.transports.Console({
+      level: 'info',
+      silent: logConsole !== 'true',
+    }))
+    errorTransports.push(new winston.transports.Console({
+      level: 'error',
+      silent: logConsole !== 'true',
+    }))
+
+    if (logFile !== 'false') {
+      let { logPath } = this
+      logPath = logFileDirname || logPath
+      if (logPath) {
+        if (!fs.existsSync(logPath)) {
+          mkdirp.sync(logPath)
+        }
+
+        accessTransports.push(new (winston.transports as DailyRotateFile).DailyRotateFile({
+          filename: logFileAccessFilename || `%DATE%-${logType}.access.log`,
+          dirname: logPath,
+          datePattern: logFileDatePattern || 'YYYY-MM-DD-HH',
           zippedArchive: true,
           level: 'info',
-        }),
-      ],
+          maxSize: logFileMaxSize || null,
+          maxFiles: logFileMaxFiles || '30d',
+        }))
+
+        errorTransports.push(new (winston.transports as DailyRotateFile).DailyRotateFile({
+          filename: logFileErrorFilename || `%DATE%-${logType}.error.log`,
+          dirname: logPath,
+          datePattern: logFileDatePattern || 'YYYY-MM-DD-HH',
+          zippedArchive: true,
+          level: 'error',
+          maxSize: logFileMaxSize || null,
+          maxFiles: logFileMaxFiles || '30d',
+        }))
+      }
+    }
+    const defaultMeta = logMetaService ? { service: logMetaService } : null
+    const accessLog = winston.createLogger({
+      defaultMeta,
+      transports: accessTransports,
     })
 
     const errorLog = winston.createLogger({
-      transports: [
-        new (winston.transports as DailyRotateFile).DailyRotateFile({
-          filename: path.resolve(logPath, `./%DATE%-${logType}.error.log`),
-          datePattern: `YYYY-MM-DD-HH`,
-          zippedArchive: true,
-          level: 'error',
-        }),
-      ],
+      defaultMeta,
+      transports: errorTransports,
     })
 
     this.cache.set(logType, {
